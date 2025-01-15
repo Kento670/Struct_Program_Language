cef9ced2c8aa3b5e1f882b8946f7d78e15898fc8
Kent Ogasawara <kent.ogasawara30@gmail.com>
2025-01-15 21:41:24 +0000

    changes

--- topic-01-integers/eval.py
+++ topic-01-integers/eval.py
@@ -2,18 +2,28 @@
 
 def eval(s):
     for c in s:
-        assert c in "-0123456789"
-
+        assert c in "-0123456789."
     n = 0
     if s[0] == '-':
         sign = -1
         s = s[1:]
     else:
         sign = 1
-
+    multi = 1.0
+    fractional = False
     assert len(s) > 0
+    assert s != '.'
     while len(s) > 0:
-        n = n * 10 + ord(s[0]) - ord("0")
+        assert s[0] != '-'
+        if s[0] == ".":
+            assert fractional == False
+            fractional == True
+        else:
+            if not fractional:
+                n = n * 10 + ord(s[0]) - ord("0")
+            else:
+                multi = multi / 10
+                n = n + (ord(s[0]) - ord("0")) * multi
         s = s[1:]
 
     return n * sign
@@ -29,6 +39,19 @@ def test_eval():
     assert eval("1099") == 1099
     assert eval("0001") == 1
     assert eval("-99") == -99
+    assert eval("1.") == 1
+    assert eval ("1.23") == 1.23
+    assert eval ("-1.23") == -1.23
+
+    try:
+        eval("1.23")
+        assert False, "No error for 1..2"
+    try:
+        eval ("--1")
+        assert False, "no error for [--1]"
+    except Exception as e:
+            print("got an error for [--1]")
+    
 
 
 if __name__ == "__main__":
